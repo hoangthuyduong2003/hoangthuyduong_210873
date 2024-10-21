ebc78d0cc2b8d71b58630be911782bcd9deaf435
hoangthuyduong <hoangthuyduong.email@example.com>
2024-10-21 16:29:16 +0700

    Tao 5 file user2_N.txt va them noi dung vao file N le

--- cau5.sh
+++ cau5.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
-touch info.txt
-
-echo "hoangthuyduong $USER" > info.txt
-echo "Ngay va gio hien tai: $(date)" >> info.txt
+#Tao 5 File Tu User2_1.txt Den User2_5.txt
+for i in {1..5}
+do 
+	touch user2_$i.txt
+#Them Noi Dung User2 Init Vao Cac File Co N le
+if [ $((i % 2)) -ne 0 ]; then
+echo "User 2 init" > user_$i.txt
+fi
+done
