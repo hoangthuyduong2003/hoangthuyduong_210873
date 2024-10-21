b855b8eac19a4a75135b401d81c689a79a2c5a11
hoangthuyduong <hoangthuyduong.email@example.com>
2024-10-21 22:15:20 +0700

    Add shellc1 and shellc2 with main

---
+++ shellc1.c
@@ -0,0 +1,5 @@
+//shellc1.c
+#include <stdio.h>
+void inShellc1() {
+printf ("This is shellc1\n");
+}
---
+++ shellc2.c
@@ -0,0 +1,5 @@
+//shellc2.c
+#include <stdio.h>
+void inShellc1() {
+printf ("This is shellc2\n");
+}
